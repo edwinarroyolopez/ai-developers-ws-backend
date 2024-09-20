5f0cf58dd126093ebaa3ee6261a0f43140c608c2
Ed <edwin.antonio.arroyo.lopez@gmail.com>
2024-09-20 14:09:13 -0500

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Habilitar CORS para cualquier origen
+  app.enableCors({
+    origin: '*',  // Permitir cualquier origen
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,  // Permite el uso de cookies, si es necesario
+  });
+
   await app.listen(4000);
 }
 bootstrap();
