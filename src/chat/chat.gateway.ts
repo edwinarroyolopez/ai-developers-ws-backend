bf29c83c485763b16266ae19f0a594099b042345
Ed <edwin.antonio.arroyo.lopez@gmail.com>
2024-09-20 16:07:27 -0500

    other changes

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,25 +1,41 @@
-import { WebSocketGateway, WebSocketServer, SubscribeMessage, MessageBody } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, MessageBody } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 import { ChatService } from './chat.service';
 
-@WebSocketGateway({ cors: true })  // habilitamos CORS para permitir conexiones desde diferentes orígenes
-export class ChatGateway {
+@WebSocketGateway({ cors: true })  // Habilitar CORS si es necesario
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
   constructor(private readonly chatService: ChatService) {}
 
-  // Evento para enviar mensajes a todos los usuarios
+  // Manejar cuando un cliente se conecta
+  handleConnection(client: Socket) {
+    console.log(`Cliente conectado: ${client.id}`);
+    // Aquí podrías emitir un mensaje a todos indicando que un nuevo usuario se ha unido
+    client.emit('connected', 'Bienvenido al chat');
+    
+  }
+
+  // Manejar cuando un cliente se desconecta
+  handleDisconnect(client: Socket) {
+    console.log(`Cliente desconectado: ${client.id}`);
+    // Aquí podrías emitir un mensaje a todos indicando que el usuario se fue
+    this.server.emit('disconnected', `El cliente con id ${client.id} se ha desconectado`);
+  }
+
+  // Evento para manejar el envío de mensajes
   @SubscribeMessage('sendMessage')
   async handleMessage(@MessageBody() data: { username: string; text: string }): Promise<void> {
     const message = await this.chatService.createMessage(data.username, data.text);
     this.server.emit('receiveMessage', message);
   }
 
-  // Evento para obtener todos los mensajes al conectarse
+  // Evento para obtener todos los mensajes almacenados
   @SubscribeMessage('getMessages')
-  async handleGetMessages(): Promise<void> {
+  async handleGetMessages(client: Socket): Promise<void> {
     const messages = await this.chatService.findAllMessages();
-    this.server.emit('allMessages', messages);
+    console.log('allMessages');
+    client.emit('allMessages', messages);
   }
 }
