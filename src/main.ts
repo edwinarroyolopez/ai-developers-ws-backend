14a77583fb7a4d4377cbcde77d3d6b7fd3a90979
Ed <edwin.antonio.arroyo.lopez@gmail.com>
2024-09-20 14:37:03 -0500

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Configurar CORS para un origen específico
   app.enableCors({
-    origin: 'https://ai-developers-ws-frontend-5drsn1gao-edwinarroyolopezs-projects.vercel.app', // El dominio del frontend en Vercel
+    origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,  // Permitir cookies y credenciales
   });
